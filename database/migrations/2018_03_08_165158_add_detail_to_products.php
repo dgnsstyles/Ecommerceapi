093ad65e18c01614e84c8e1853ce664a4e6e3f5f
davidgoren <dgoren@hotmail.com>
2018-03-08 14:04:58 -0300

    Seeding part for products and review

--- database/migrations/2018_03_08_165158_add_detail_to_products.php
+++ database/migrations/2018_03_08_165158_add_detail_to_products.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class AddDetailToProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::table('products', function($table) {
             $table->text('detail');
-            $table->integer('price');
-            $table->integer('stock');
-            $table->integer('discount');
-            $table->timestamps();
         });
     }
 
@@ -31,6 +25,8 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function($table){
+           $table->dropColumn('detail');
+        });
     }
 }
